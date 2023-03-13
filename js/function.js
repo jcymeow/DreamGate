cbc98cf194b4bba25e564330d198301b45533ee4
DreamCrossGate <125602461+jcymeow@users.noreply.github.com>
2023-03-14 07:17:08 +0800

    modify 任务相关

--- js/function.js
+++ js/function.js
@@ -1,1 +1,39 @@
-function setbgposition(o,x,y){o=typeof(o)=='object'?o:document.getElementById(o);if(o.style.backgroundPosition!=''){var p=o.style.backgroundPosition.split(' ');var ox=p[0],oy=p[1];}else var ox=oy=0;ox=x!='auto'?x+'px':ox;oy=y!='auto'?y+'px':oy;o.style.backgroundPosition=ox+' '+oy;}
+function initMissionPage(){
+    var id = getUrlParam("id");
+    var idx = getUrlParam("idx");
+    $.getJSON('./data/mission.json', function(list){
+        $(list).each(function(i, series){
+            if(series.id == id){
+                var sel = new Array();
+                sel.push('<select onChange="gotourl(this.value)">');
+                $(series.data).each(function(j, mission){
+                    sel.push('<option value="./');
+                    sel.push(mission.page);
+                    sel.push('?id=');
+                    sel.push(series.id);
+                    sel.push('&idx=');
+                    sel.push(j)
+                    sel.push('" ');
+                    if(j == idx){
+                        sel.push(' selected="selected" ');
+                    }
+                    sel.push('>');
+                    sel.push(mission.name);
+                    sel.push('</option>');
+                });
+                sel.push('</select>');
+                $("#crumb").html(sel.join(''));
+            }
+        });
+    })
+}
+
+function getUrlParam(name) {
+    var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)");
+    var r = window.location.search.substr(1).match(reg);
+    if (r != null) return unescape(r[2]); return null;
+}
+
+function gotourl(loca) {
+    parent.frames['main'].document.location = loca;
+}
